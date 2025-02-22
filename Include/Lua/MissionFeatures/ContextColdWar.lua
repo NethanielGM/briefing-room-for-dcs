97a077c2f322a4f2ff69e1e2c003d62de3b2388a
john681611 <john681611@hotmail.com>
2025-02-22 11:05:36 +0000

    Fix: Cold War Air ROEs

--- Include/Lua/MissionFeatures/ContextColdWar.lua
+++ Include/Lua/MissionFeatures/ContextColdWar.lua
@@ -1,17 +1,21 @@
 -- Makes units hold their fire until attacked, turning the mission into a cold war situation.
-for i = 1, 2 do
-  for _, g in pairs(coalition.getGroups(i)) do
-    local controller = g:getController()
+function briefingRoom.mission.missionFeatures.coldWarROE(args, time)
+  if briefingRoom.mission.missionFeatures.contextColdWar.wasHasStarted then return end
+  for i = 1, 2 do
+    for _, g in pairs(coalition.getGroups(i)) do
+      local controller = g:getController()
 
-    if g:getCategory() == Group.Category.AIRPLANE or g:getCategory() == Group.Category.HELICOPTER then -- AIRCRAFT
-      controller:setOption(AI.Option.Air.id.ROE, AI.Option.Air.val.ROE.RETURN_FIRE)                    -- never attack or return fire
-    elseif g:getCategory() == Group.Category.GROUND then                                               -- GROUND VEHICLES
-      controller:setOption(AI.Option.Ground.id.ALARM_STATE, AI.Option.Ground.val.ALARM_STATE.RED)      -- still search for enemies with radar even if not attacking
-      controller:setOption(AI.Option.Ground.id.ROE, AI.Option.Ground.val.ROE.RETURN_FIRE)              -- never attack or return fire
-    elseif g:getCategory() == Group.Category.SHIP then                                                 -- SHIPS
-      controller:setOption(AI.Option.Naval.id.ROE, AI.Option.Naval.val.ROE.RETURN_FIRE)                -- never attack or return fire
+      if g:getCategory() == Group.Category.AIRPLANE or g:getCategory() == Group.Category.HELICOPTER then -- AIRCRAFT
+        controller:setOption(AI.Option.Air.id.ROE, AI.Option.Air.val.ROE.RETURN_FIRE)                    -- never attack or return fire
+      elseif g:getCategory() == Group.Category.GROUND then                                               -- GROUND VEHICLES
+        controller:setOption(AI.Option.Ground.id.ALARM_STATE, AI.Option.Ground.val.ALARM_STATE.RED)      -- still search for enemies with radar even if not attacking
+        controller:setOption(AI.Option.Ground.id.ROE, AI.Option.Ground.val.ROE.RETURN_FIRE)              -- never attack or return fire
+      elseif g:getCategory() == Group.Category.SHIP then                                                 -- SHIPS
+        controller:setOption(AI.Option.Naval.id.ROE, AI.Option.Naval.val.ROE.RETURN_FIRE)                -- never attack or return fire
+      end
     end
   end
+  return time + 10
 end
 
 briefingRoom.mission.missionFeatures.contextColdWar = {}
@@ -48,3 +52,4 @@ end
 
 -- Enable event handler
 world.addEventHandler(briefingRoom.mission.missionFeatures.contextColdWar.eventHandler)
+timer.scheduleFunction(briefingRoom.mission.missionFeatures.coldWarROE, {}, timer.getTime() + 1)
